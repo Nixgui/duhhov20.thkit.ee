ef5bb9678a1f88877124fd7128c1d4fe043c241e
Nixgui <robert.ecward@gmail.com>
2021-05-24 22:00:20 +0300

    Added effects

--- minuRuhm/dozd.js
+++
@@ -1,7 +0,0 @@
-let COLOR_SPACE = "black";
-let COLOR_START = "white";
-let STAR_NUM = 200;
-let STAR_SIZE = 0.005;
-let STAR_SPEED = 0.05;
-
-let canvas = document.getElementById("canvas");
---
+++ minuRuhm/rainbow.js
@@ -0,0 +1,48 @@
+//initial
+let c = document.getElementById("canvas"),
+	w = c.width = window.innerWidth,
+	h = c.height = window.innerHeight / 4,
+	ctx = c.getContext('2d'),
+
+	//parameters
+	total = w,
+	accelleration = .02,
+
+	//afterinitial calculations
+	size = w/total,
+	occupation = w/total,
+	repaintColor = 'rgba(0, 0, 0, .04)'
+colors = []
+	dots = []
+	dotsVel = [];
+
+//setting the colors' hue
+//and y level for all dots
+let portion = 360/total;
+for(let i = 0; i < total; ++i){
+	colors[i] = portion * i;
+
+	dots[i] = h;
+	dotsVel[i] = 10;
+}
+
+function anim(){
+	window.requestAnimationFrame(anim);
+
+	ctx.fillStyle = repaintColor;
+	ctx.fillRect(0, 0, w, h);
+
+	for(let i = 0; i < total; ++i){
+		let currentY = dots[i] - 1;
+		dots[i] += dotsVel[i] += accelleration;
+
+		ctx.fillStyle = 'hsl('+ colors[i] + ', 80%, 50%)';
+		ctx.fillRect(occupation * i, currentY, size, dotsVel[i] + 1);
+
+		if(dots[i] > h && Math.random() < .01){
+			dots[i] = dotsVel[i] = 0;
+		}
+	}
+}
+
+anim();
